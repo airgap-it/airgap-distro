a857f378259a4435723f341200679e8ce6504d42
Alessandro De Carli <a.decarli@papers.ch>
2018-08-16 16:42:21 +0200

    feat(airgap): adds airgap vault support

--- overlay/usr/local/src/my-ether-wallet-dist-v3.9.9.5/bin/startMEW.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-
-var open = require("open");
-var seperator = process.platform=="win32" ? "\\" : "/";
-open(require('path').dirname(require.main.filename)+seperator+".."+seperator+"index.html");
